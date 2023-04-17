341b8dc29e4bd7c461adc81130940c91e04e5673
kvalaroutsos <66298082+kvalaroutsos@users.noreply.github.com>
2023-04-17 18:28:00 +0300

    Delete game_over.py

--- game_over.py
+++
@@ -1,9 +0,0 @@
-from turtle import Turtle
-ALIGNMENT='center'
-FONT=('Arial', 16, 'normal')
-
-class Game_over(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.color('white')
-        self.write('GAME OVER',align=ALIGNMENT, font=FONT)

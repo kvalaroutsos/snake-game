d7af49b3a0c5cfff59547458be727dae36d7c623
kvalaroutsos <66298082+kvalaroutsos@users.noreply.github.com>
2023-04-17 15:35:01 +0300

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
-from snake import Snake
 import time
+from turtle import Screen
+
+from food import Food
+from snake import Snake
 
 screen=Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,13 @@ screen.title('My Snake Game')
 screen.tracer(0)
 
 snake=Snake()
+food=Food()
+
+screen.listen()
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.left,'Left')
+screen.onkey(snake.right,'Right')
 
 game_is_on= True
 while game_is_on:
@@ -16,6 +25,10 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    #Detect coalition with food
+    if snake.head.distance(food)<15:
+        print('nom nom nom ')
 
 
 
--- snake.py
+++ snake.py
@@ -4,6 +4,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head=self.segments[0]
 
     def create_snake(self):
 
@@ -20,4 +21,21 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() !=180:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() !=180:
+            self.head.setheading(0)
+
